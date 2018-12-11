97c6d55338780017191568305da07a35e20a6521
HenriLindret <henri.lindret@khk.ee>
2018-12-11 08:39:40 +0200

    modified file `wflogs` (after activation of `WPForms Lite` version 1.5.0.3)

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1544777911;s:7:"authKey";s:64:"P`^Abk9V<EPKQ qKZGV!RfL)5!#DiIE[.>^rR8VEtzyo]^t:{.f gO3;OY8-[S(1";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:810;}
